b65a169c074aee57c569b2aeedbe7cef358dc386
Alish Giri <alishgiri.92@gmail.com>
2019-10-21 21:39:11 +0545

    basic slide up view created

--- App.js
+++ App.js
@@ -1,114 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  Button,
+  Animated,
+  Dimensions,
+  SafeAreaView,
+  TouchableWithoutFeedback,
 } from 'react-native';
+import React from 'react';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import {generateFontistoIcon} from './lib/utilities/icon-generator';
+import {AppButton} from './lib/components/ui';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const {height} = Dimensions.get('window');
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#ebedeb',
+  },
+  slideup_container: {
+    flex: 1,
+    alignItems: 'center',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 15,
+    marginVertical: 20,
+    fontWeight: 'bold',
+  },
+  fab_top_left: {
+    top: 20,
+    left: 20,
+    position: 'absolute',
+  },
+  commenting_button_background: {
+    top: -20,
+    right: 30,
+    width: 60,
+    height: 60,
+    shadowRadius: 8,
+    borderRadius: 30,
+    shadowOpacity: 0.6,
+    alignItems: 'center',
+    position: 'absolute',
+    shadowColor: 'orange',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+    borderBottomRightRadius: 0,
+    shadowOffset: {width: 0, height: 4},
+  },
+  commenting_button_text: {
+    transform: [{scaleX: -1}, {rotate: '0deg'}],
+  },
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Button
+          title="Show panel"
+          style={styles.fab_top_left}
+          onPress={() => this._panel.show()}
+        />
+        <SlidingUpPanel
+          draggableRange={{top: height / 1.75, bottom: 70}}
+          animatedValue={new Animated.Value(70)}
+          ref={c => (this._panel = c)}>
+          <View style={styles.slideup_container}>
+            <Text style={styles.text}>EXPLORE</Text>
+
+            <AppButton
+              onPress={() => this._panel.hide()}
+              titleStyle={styles.commenting_button_text}
+              style={styles.commenting_button_background}
+              title={generateFontistoIcon('commenting', 20, 'white')}
+            />
+          </View>
+        </SlidingUpPanel>
+      </SafeAreaView>
+    );
+  }
+}
 
 export default App;
