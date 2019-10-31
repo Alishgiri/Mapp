790a7d1015d0dfb461c0d88f48717143c4e22066
Alish Giri <alishgiri.92@gmail.com>
2019-10-31 09:29:28 +0545

    photo gallery added to the app to select images for the cover photo

--- android/app/src/main/java/com/mapp/MainApplication.java
+++ android/app/src/main/java/com/mapp/MainApplication.java
@@ -14,6 +14,7 @@ import com.reactnativenavigation.react.ReactGateway;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.imagepicker.ImagePickerPackage;
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -43,7 +44,8 @@ public class MainApplication extends NavigationApplication {
             // eg. new VectorIconsPackage()
             new LinearGradientPackage(),
             new MapsPackage(),
-            new ImagePickerPackage()
+            new ImagePickerPackage(),
+            new CameraRollPackage()
         );
     }
 
